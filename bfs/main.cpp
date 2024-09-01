04cafa88527ca8ae7deb743e7ee8db70c8de2f9c
marwa ayman <i.am.marwa.ayman@gmail.com>
2024-09-01 08:11:30 +0300

    add dijkstra function

--- bfs/main.cpp
+++ bfs/main.cpp
@@ -1,29 +1,22 @@
-#include<iostream>
-#include<cmath>
-#include<algorithm>
-#include<string>
-#include<vector>
-#include<queue>
-#include<stack>
-#include<map>
-#include<set>
+#include <bits/stdc++.h>
 #define mr ios_base::sync_with_stdio(0);
 #define prince cin.tie(0),cout.tie(0)
 #define the_forgotten_line freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
 #define all_list list.begin(), list.end()
-#define all_ans ans.begin(), ans.end()
-#define all_out out.begin(), out.end()
 #define endl "\n"
-#define esle else 
 #define ll long long 
-#define itn int
 using namespace std;
 const long long N = 200;
+const long long INF = 1e18;
 
 vector<vector<int>> adj(N);
+vector<vector<pair<int, int>>> adj_w(N);
 vector<bool> visited(N);
 vector<int> level(N);
-void reset(int n) {
+vector<int> path(N,-1);
+vector<long long> dis(N,INF);
+void reset(int n)
+{
     for (int i = 0; i < n; i++) {
         adj[i].clear();
         visited[i] = false;
@@ -74,17 +67,45 @@ bool is_bipartit(int scr)
     }
     return 1;
 }
+void Dijkstra(int src){
+    priority_queue < pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    pq.push({0, src});
+    dis[src] = 0;
+    while(!pq.empty()){
+        int node = pq.top().second;
+        int cost = pq.top().first;
+        pq.pop();
+        if(visited[node])continue;
+        visited[node] = 1;
+        for (auto [a, b] : adj_w[node]){
+            if(cost + b < dis[a]){
+                pq.push({cost + b, a});
+                dis[a] = cost + b;
+                path[a] = node;
+            }
+        }
+    }
+}
+void get_path(int node){
+    if(node == -1)
+        return;
+    get_path(path[node]);
+    cout << node << " ";
+}
 void solve()
 {
     int n, m;
     cin >> n >> m;
     for (int i = 0; i < m; i++)
     {
-      int u, v;
-      cin >> u >> v;
-      adj[u].push_back(v);
-      adj[v].push_back(u);
+        int u, v, w;
+        cin >> u >> v >> w;
+        adj_w[u].push_back({v, w});
+        adj_w[v].push_back({u, w});
     }
+    Dijkstra(1);
+    get_path(n);
+    cout << endl;
 }
 int main()
 {
